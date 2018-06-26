9fe578f2ff23fb1b62cac932a60d1726b30a8e97
ashok10103 <ashok10103@gmail.com>
2018-06-26 17:26:10 +0530

    changed model

--- rest_api_app/models.py
+++ rest_api_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class BucketList(models.Model):
     name = models.CharField(max_length=250,blank=False,unique=True)
+    owner=models.ForeignKey('auth.User',related_name='bucket_lists',on_delete=models.CASCADE)
     date_created =  models.DateTimeField(auto_now_add=True)
     date_modified =  models.DateTimeField(auto_now=True)
 
