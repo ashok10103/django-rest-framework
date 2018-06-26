a4b97742ebf7ba8d65e59df2c3a23b1959f58654
ashok10103 <ashok10103@gmail.com>
2018-06-26 18:39:35 +0530

    completed auth permissions setup

--- rest_api_app/urls.py
+++ rest_api_app/urls.py
@@ -5,6 +5,7 @@ from .views import HomeView,DetailsView
 #The format_suffix_pattern allows us to specify the data format (raw json or even html) when we use the URLs. It appends the format to be used to every URL in the pattern.
 
 urlpatterns = [
+      url(r'^auth/',include('rest_framework.urls',namespace='rest_framework')), 
       url(r'^bucketlists/$', HomeView.as_view(), name="home"),
       url(r'^bucketlists/(?P<pk>[0-9]+)/$',DetailsView.as_view(), name="details"),
 ]
