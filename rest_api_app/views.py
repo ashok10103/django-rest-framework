5d97ef5e600fae8273e5c9d8615ccb539a5db4f1
ashok10103 <ashok10103@gmail.com>
2018-06-26 15:55:55 +0530

    added api views

--- rest_api_app/views.py
+++ rest_api_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import BucketListSerializer
+from .models import BucketList
 # Create your views here.
+
+
+class HomeView(generics.ListCreateAPIView):
+    queryset = BucketList.objects.all()
+    serializer_class = BucketListSerializer
+
+    def perform_create(self,serializer):
+        serializer.save()
